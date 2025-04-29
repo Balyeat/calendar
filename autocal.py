bddf4d6b59a7106e76e3fd99776608ff77c8c556
Balyeet <32548112+Balyeat@users.noreply.github.com>
2025-04-29 13:22:52 +0000

    added assignment

--- autocal.py
+++ autocal.py
@@ -14,8 +14,6 @@ def clean_title(title):
     cleaned_title = re.sub(r'Forelæsning/lecture', r'Lecture', cleaned_title)
     cleaned_title = re.sub(r'Teoretiske øvelser/theoretical exercises', r'Exercise', cleaned_title)
     cleaned_title = cleaned_title.strip()
-    print(f"Origina title: {title}")
-    print(f"Cleaned title: {cleaned_title}")
     return cleaned_title
 
 def download_ical(url, filename):
@@ -30,11 +28,10 @@ def download_ical(url, filename):
         return None
     return filename
 
-def process_ical(input_file, output_file):
-    """Process the iCal file to clean event titles."""
+def process_ical_lectures_and_exercises(input_file, output_file):
+    """Process the iCal file to clean event titles for lectures and exercises."""
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
         in_event = False
-        # REMOVE aktivitet and beskrivelse from DESCRIPTION
         for line in infile:
             if line.startswith("BEGIN:VEVENT"):
                 in_event = True
@@ -42,10 +39,7 @@ def process_ical(input_file, output_file):
                 in_event = False
 
             elif in_event and line.startswith("DESCRIPTION:"):
-                # Clean the description
                 original_description = line[12:].strip()
-                # Example: Remove unwanted text
-                # cleaned_description = re.sub(r'Aktivitet[^\n]*\n', '', original_description)
                 cleaned_description = re.sub(r'Aktivitet.*?\\n', '', original_description)
                 cleaned_description = re.sub(r'Beskrivelse.*?\\n', '', cleaned_description)
                 cleaned_description = re.sub(r'Undervisningstype.*?\\n', '', cleaned_description)
@@ -54,28 +48,62 @@ def process_ical(input_file, output_file):
                 cleaned_description = re.sub(r'Hold / Student Set', 'Group', cleaned_description)
                 line = f"DESCRIPTION:{cleaned_description}\n"
 
-                print(f"Cleaned description: {line}")
-    
             elif line.startswith("LOCATION:"):
-                # Clean the location
                 original_location = line[9:].strip()
-                # Example: remove all characters up to the first - and including the first - + " "
                 cleaned_location = re.sub(r'^[^-]*- ', '', original_location)
                 line = f"LOCATION:{cleaned_location}\n"
 
             if in_event and line.startswith("SUMMARY:"):
-                # Clean the title
                 original_title = line[8:].strip()
-                # Example: Remove course codes and other unwanted text
-
                 cleaned_title = clean_title(original_title)
                 line = f"SUMMARY:{cleaned_title}\n"
-            
+
             outfile.write(line)
+
+def process_ical_assignments(input_file, output_file):
+    """Process the iCal file to clean and shorten event titles for assignments."""
+    with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
+        in_event = False
+        buffer = ""
+        for line in infile:
+            if line.startswith("SUMMARY"):
+                in_event = True
+                buffer = line[:-1]
+                continue
+            if in_event:
+                in_event = False
+                if line[0] == " ":
+                    buffer += line[1:]
+                # shortened title is
+                buffer = buffer[8:].strip()
+                shortened_title = re.split(r"\[", buffer)
+                
+                #remove all character until ; using regex and remove ]
+                shortened_title[1] = re.sub(r'^[^;]*;', '', shortened_title[1])
+                shortened_title[1] = re.sub(r'\]', '', shortened_title[1])
+            
+                shortened_title[1] = re.sub(r'Implementering af programmeringssprog', r'IPS', shortened_title[1])
+                shortened_title[1] = re.sub(r'Databases and Information Systems', r'DIS', shortened_title[1])
+                shortened_title[1] = re.sub(r'Algoritmer og Datastrukturer', r'AD', shortened_title[1])
+
+                shortened_title = shortened_title[1] + " - " + shortened_title[0]
+
+                line = f"SUMMARY:{shortened_title}\n"
+
+            outfile.write(line)
+                           
+            
+
 # Usage
 ical_url = "https://personligtskema.ku.dk/ical.asp?objectclass=student&id=bgd215"
-downloaded_ical = download_ical(ical_url, "latest_calendar.ics")
+assignments_url = "https://absalon.instructure.com/feeds/calendars/user_xsBGWlGBZ7dnHadz84HVlc0jxoz3oUgGo7klFwJG.ics"
 
-if downloaded_ical:
-    process_ical(downloaded_ical, "cleaned_calendar.ics")
-    print("Processed iCal file saved as cleaned_calendar.ics")
+downloaded_ical_lectures = download_ical(ical_url, "latest_calendar_lectures.ics")
+if downloaded_ical_lectures:
+    process_ical_lectures_and_exercises(downloaded_ical_lectures, "cleaned_calendar_lectures.ics")
+    print("Processed iCal file for lectures and exercises saved as cleaned_calendar_lectures.ics")
+
+downloaded_ical_assignments = download_ical(assignments_url, "latest_calendar_assignments.ics")
+if downloaded_ical_assignments:
+    process_ical_assignments(downloaded_ical_assignments, "cleaned_calendar_assignments.ics")
+    print("Processed iCal file for assignments saved as cleaned_calendar_assignments.ics")
